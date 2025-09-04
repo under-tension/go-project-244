cc79a5291cb8e844ce203f1247da9f7f14e07e9d
hermit <denis@localhost.localdomain>
2025-09-04 23:13:27 +0300

    feat: add print difference in json format

---
+++ pkg/formatters/json.go
@@ -0,0 +1,55 @@
+package formatters
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+type JsonFormatter struct {
+	BaseFormatter
+}
+
+func (f JsonFormatter) Format(diffTree []DiffTree) (string, error) {
+	f.settings = Settings{StartIdents: 0}
+	return f.format(diffTree, f.settings.StartIdents)
+}
+
+func (f JsonFormatter) format(diff []DiffTree, depth int) (string, error) {
+	result := ""
+	result += "{\n"
+
+	for _, node := range diff {
+
+		if node.Type == TYPE_ROOT && IsDiffTreeSlice(node.Val) {
+			subTreeResult, err := f.format(node.Val.([]DiffTree), depth+1)
+
+			if err != nil {
+				return "", err
+			}
+
+			result += strings.Repeat("\t", depth+1) + node.Name + ": " + subTreeResult
+		} else {
+			oldValEncoded, _ := json.Marshal(node.OldVal)
+			valEncoded, _ := json.Marshal(node.Val)
+
+			result += strings.Repeat("\t", depth+1)
+
+			switch node.Status {
+			case STATUS_ADDED:
+				result += "+ " + node.Name + ": " + string(valEncoded) + "\n"
+			case STATUS_DELETED:
+				result += "- " + node.Name + ": " + string(oldValEncoded) + "\n"
+			case STATUS_NON_CHANGE:
+				result += "  " + node.Name + ": " + string(valEncoded) + "\n"
+			case STATUS_UPDATED:
+				result += "- " + node.Name + ": " + string(oldValEncoded) + "\n"
+				result += strings.Repeat("\t", depth+1)
+				result += "+ " + node.Name + ": " + string(valEncoded) + "\n"
+			}
+		}
+	}
+
+	result += strings.Repeat("\t", depth) + "}\n"
+
+	return result, nil
+}
